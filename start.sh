127fb4d2a82c765686a0d1fe5430590ca7b54564
Nandeppa <118658642+Nandeppa@users.noreply.github.com>
2023-02-06 13:17:59 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/lazyindu/vk.git /LazyPrincess
+  git clone https://github.com/Nandeppa/vk-1 /LazyPrincess
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /LazyPrincess
